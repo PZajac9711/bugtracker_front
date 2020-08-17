19be7a9eaf6f0131d7f9e251c3b4f3a5719bba29
PZajac9711 <patzaj9711@gmail.com>
2020-08-17 10:46:36 +0200

    GOD PLX NO MORE FIX HEROKU!

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/bugtracker'));
 
 app.listen(process.env.PORT || 8080);
 
 
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/bugtracker/index.html'));
+})
 
 console.log('Console listening!');
